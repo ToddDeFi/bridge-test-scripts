d1e47f035527509e767ff95f238e265c36e6212e
ToddDeFi <102527508+ToddDeFi@users.noreply.github.com>
2023-09-25 21:38:30 +0200

    added input

--- bridgeTest.js
+++ bridgeTest.js
@@ -1,9 +1,25 @@
 const { ethers } = require('ethers');
+const readline = require('readline'); 
+
 const distributorContractAbi = require('./abi/distributor.json');
 const erc20ContractAbi = require('./abi/erc-20.json');
 
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+async function getUserPrivateKey() {
+  return new Promise((resolve) => {
+    rl.question('Введите приватный ключ: ', (privateKey) => {
+      resolve(privateKey);
+      rl.close();
+    });
+  });
+}
+
 async function distributeFundsAndTokens() {
-  const mainWalletPrivateKey = '9f04357fe1e6792e3faa865a4823a334da10fa2ebd46aa3621aa784ca382f9c7';
+  const mainWalletPrivateKey = await getUserPrivateKey(); 
   const provider = new ethers.providers.JsonRpcProvider('https://bsc-testnet.blockpi.network/v1/rpc/public');
   const mainWallet = new ethers.Wallet(mainWalletPrivateKey, provider);
 
@@ -13,14 +29,12 @@ async function distributeFundsAndTokens() {
   const stablePlusTokenAddress = '0x6Af0c089b809a0E08cf84c6538A46C17dF234Ab3';
   const tokenContract = new ethers.Contract(stablePlusTokenAddress, erc20ContractAbi, mainWallet);
 
-  // Минт 10 stable+
   const tokenAmountToMint = ethers.utils.parseUnits('10', 'ether');
   const mintTx = await tokenContract.mint(mainWallet.address, tokenAmountToMint);
   await mintTx.wait();
   
-  console.log('Успешно создано 10 Stable+ токенов.', mintTx.hash);
+  console.log('Успешно создано 10 Stable+ токенов. Хэш транзакции:', mintTx.hash);
 
-  // Создание 10 аккаунтов
   const recipients = [];
 
   for (let i = 0; i < 10; i++) {
@@ -29,7 +43,6 @@ async function distributeFundsAndTokens() {
     recipients.push(newWalletAddress);
   }
 
-  // Апрув и отправка на них 0.001 BNB и 10 Stable+
   const valueToSend = ethers.utils.parseUnits('0.001', 'ether');
   const tokenAmount = ethers.utils.parseUnits('10', 'ether');
 
